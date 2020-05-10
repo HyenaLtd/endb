8f402e1fe08cb9804ea6684b9e319b03d9c0781b
Ayyan Lewis <ayyanlewis@gmail.com>
2020-05-10 23:46:42 +0400

    test: add error event tests (#78)

--- test/functions.js
+++ test/functions.js
@@ -188,7 +188,7 @@ const valueTest = (test, Endb, options) => {
   });
 };
 
-const adapterTest = (it, Endb, goodUri) => {
+const adapterTest = (it, Endb, goodUri, badUri) => {
   describe('Adapter Test', () => {
     it('should infer the adapter from the URI', async () => {
       const endb = new Endb(goodUri);
@@ -197,6 +197,13 @@ const adapterTest = (it, Endb, goodUri) => {
       await endb.set('foo', 'bar');
       expect(await endb.get('foo')).toBe('bar');
       await endb.clear();
+    });
+
+    it('should emit connection errors', done => {
+      const endb = new Endb(badUri);
+      endb.on('error', () => {
+        done();
+      });
     });
   });
 };
