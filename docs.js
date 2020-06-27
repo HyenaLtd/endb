ee3966fe4037a729acab2eedd0a5867d13f74c5b
chroventer <ayyanlewis@gmail.com>
2020-06-26 23:50:15 -0700

    chore: clean up

--- docs.js
+++ docs.js
@@ -1,39 +1,51 @@
-const Docma = require("docma");
+const Docma = require('docma');
+const fs = require('fs');
+
+const packages = fs.readdirSync('./packages');
 
 const config = {
-  dest: "./docs",
+  dest: './docs',
   debug: 16,
   clean: true,
   app: {
-    title: "Endb",
+    title: 'Endb',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width,initial-scale=1.0" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width,initial-scale=1.0' },
     ],
-    base: "/",
-    entrance: "content:readme",
+    base: '/',
+    entrance: 'content:readme',
     routing: Docma.RoutingMethod.PATH,
     server: Docma.ServerType.GITHUB,
   },
-  src: ["packages/**/src/*.js", "./README.md"],
+  src: [
+    './packages/**/src/*.js',
+    { content: './README.md' },
+    ...packages.map((package) => ({
+      [package]: `./packages/${package}/README.md`,
+    })),
+  ],
   template: {
     options: {
-      title: "Endb",
+      title: 'Endb',
       navbar: {
+        dark: true,
         menu: [
-          { label: "Home", href: "/" },
-          { label: "Documentation", href: "/api", iconClass: "fas fa-book" },
+          { label: 'Home', href: '/' },
           {
-            label: "GitHub",
-            href: "https://github.com/chroventer/endb",
-            iconClass: "fab fa-github",
-            target: "_blank",
+            label: 'Packages',
+            items: packages.map((package) => ({
+              label: package,
+              href: `/${package}`,
+            })),
+          },
+          {
+            label: 'GitHub',
+            href: 'https://github.com/chroventer/endb',
+            iconClass: 'fab fa-github',
+            target: '_blank',
           },
         ],
-      },
-      sidebar: {
-        enabled: true,
-        outline: "tree",
       },
     },
   },
@@ -42,5 +54,5 @@ const config = {
 
 Docma.create()
   .build(config)
-  .then(() => console.log("Successfully build the documentation."))
+  .then(() => console.log('Successfully build the documentation.'))
   .catch(console.error);
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/packages'],
+  verbose: true,
+};
