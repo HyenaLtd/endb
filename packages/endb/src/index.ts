1f74d9f5633c167deb6b1e0ffbeca7e21530af15
chroventer <ayyanlewis@gmail.com>
2020-09-15 01:22:34 -0700

    refactor(endb): remove dynamic imports

--- packages/endb/src/index.ts
+++ packages/endb/src/index.ts
@@ -37,7 +37,7 @@ const load = <TVal>(
 
   if (validAdapters.includes(adapter)) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const Adapter = require(adapters[adapter]()).default;
+    const Adapter = adapters[adapter]().default;
     return new Adapter(options);
   }
 
